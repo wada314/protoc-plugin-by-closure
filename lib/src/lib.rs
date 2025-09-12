1231fb484a756565a93ad2d8227ea87860e7c0d3
Shohei Wada <pc@wada314.jp>
2025-09-12 17:37:23 +0900

    fix: include_str! to support case sensitive file system

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![doc = include_str!("../README.md")]
+#![doc = include_str!("../readme.md")]
 
 use ipc_channel::ipc::{IpcBytesReceiver, IpcBytesSender, IpcOneShotServer};
 use std::env;
